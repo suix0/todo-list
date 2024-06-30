d841ebcd51955b96ebccd5bb159b507c69f4b3a3
suix0 <112095627+suix0@users.noreply.github.com>
2024-06-30 14:12:46 +0800

    refactored code for adding task and creating form

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,32 +1,48 @@
 import task from "./index.js";
-import { openModal, closeModal, displayTasks } from "./dom.js";
-
-const modal = document.querySelector("[data-modal]");
-const taskForm = document.querySelector("form");
-const tasksContainer = document.querySelector(".tasks");
-
-// Create logic here to call for a modal to input a tasks' detail
-export function formAddTaskDetails(event) {
-  event.preventDefault();
-
-  const elements = taskForm.elements;
-  const taskTitle = elements['taskTitle'].value;
-  const taskDescription = elements['taskDescription'].value;
-  const taskDueDate = elements['taskDueDate'].value;
-  const taskPriority = parseInt(elements['taskPriority'].value);
-
-  const newTask = task(taskTitle, taskDescription, taskDueDate, taskPriority);
-
-  taskForm.reset();
-  closeModal(modal);
-
-  displayTasks(tasksContainer, newTask);
-}
+import { openModal, closeModal, displayTasks, createForm } from "./dom.js";
 
 export default function createTodo() {
+  // Dynamically create the form
+  createForm();
+
+  const addTaskForm = document.querySelector("form");
+  const modal = document.querySelector("dialog");
+  const taskContainer = document.querySelector('.tasks');
+
+  // Open the form's modal
   openModal(modal);
-  if (!taskForm.dataset.listenerAdded) {
-    taskForm.addEventListener('submit', formAddTaskDetails);
-  }
-  taskForm.dataset.listenerAdded = true;
+
+  // Add Event Listener to form upon submit
+  addTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    // Save the form input from user and store in variables
+    const elements = addTaskForm.elements;
+    
+    const taskTitle = elements.taskTitle.value;
+    const taskDescription = elements.taskDescription.value;
+    const taskDueDate = elements.taskDueDate.value;
+    const taskPriority = elements.taskPriority.value;
+    
+    // Create task based on user input
+    const newTask = task(taskTitle, taskDescription, taskDueDate, taskPriority);
+    displayTasks(taskContainer, newTask);
+  
+  
+    // Close form
+    closeModal(modal);
+  
+    // Remove the Form 
+    addTaskForm.remove();
+    modal.remove();
+  });
+  
 }
+
+// function submitAddForm(event, taskForm) {
+//   event.preventDefault();
+
+//   const taskFormElements = taskForm.elements;
+
+//   const title = taskFormElements.taskTitle;
+// }
